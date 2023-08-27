eae4128546c5e5bb6da22dd22ca4cff0c746099e
Gungun diwakar <120329859+Gungun45@users.noreply.github.com>
2023-08-27 13:55:57 +0530

    Add files via upload

---
+++ Registration form/register.js
@@ -0,0 +1,13 @@
+
+      function showInput() {
+          document.getElementById('display').innerHTML = 
+                      document.getElementById("user_input").value;
+        document.getElementById('email').innerHTML = 
+                      document.getElementById("em").value;
+                      document.getElementById('bir').innerHTML = document.getElementById("wb").value;
+                      document.getElementById('r').innerHTML = document.getElementById("bh").value;
+                      document.getElementById('p').innerHTML = document.getElementById("password").value;
+                    
+                      
+      }
+    
